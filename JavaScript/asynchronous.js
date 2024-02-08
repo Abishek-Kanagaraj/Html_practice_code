922aece41e5ab3c7c3ab56ffdc60cc88b675be17
Abishek-Kanagaraj <abishek.kanagaraj@aspiresys.com>
2024-02-08 18:22:42 +0530

    08/02/2024 Update - Asynchronous JavaScript

--- JavaScript/asynchronous.js
+++ JavaScript/asynchronous.js
@@ -1,4 +1,4 @@
-// Synchronous
+/*// Synchronous
 console.log("First");
 console.log("Second");
 console.log("Third");
@@ -18,7 +18,49 @@ const myPromise = new Promise((resolve, reject) => {
         reject("reject the promise");
     }
 })
-console.log(myPromise);
 myPromise.then(value => {
     return value
-}).then(newValue => { console.log(newValue) })
+})
+    .then(newValue => { console.log(newValue) })
+    .catch(err => {
+        console.log(`Error occured:${err}`);
+    })
+// setTimeout
+const nextPromise = new Promise((resolve, reject) => {
+    setTimeout(function () {
+        resolve("nextPromise Resolved!");
+    }, 3000)
+})
+nextPromise.then(value=>{
+    console.log(value);
+})*/
+const promise1 = fetch("https://json-placeholder.mock.beeceptor.com/users");
+const promise2 = fetch("https://json-placeholder.mock.beeceptor.com/posts");
+const promise3 = fetch("https://json-placeholder.mock.beeceptor.com/comments")
+
+// const users = fetch("https://json-placeholder.mock.beeceptor.com/users");
+Promise.all([promise1, promise2, promise3])
+    .then((responses) => {
+        for (const response of responses) {
+            console.log(response);
+        }
+    })
+    .catch((error) => {
+        console.error(`Failed to fetch: ${error}`);
+    });
+
+/* users.then(response => {
+    return response.json();
+}).then(users => {
+    console.log(users);
+})
+console.log(users);*/
+
+// Async /Await
+const users = async () => {
+    const response = await fetch("https://json-placeholder.mock.beeceptor.com/users");
+    const userData = await response.json();
+    console.log(userData); 
+    return userData;
+}
+users()
